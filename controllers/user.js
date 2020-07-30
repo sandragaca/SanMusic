8276ebc179e19ed273d94a27862419a0aa057cdb
sandragaca <sandra.garciacascante@gmail.com>
2020-07-30 18:09:58 -0500

    Funciones

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,69 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt-nodejs");
+
+
+
 function pruebas(req, res){
     res.status(200).send({
         message:'funciona el controlador de prueba'
     })
+};
+
+function create(req, res){
+    const newUser = new User();
+    const paramsBody = req.body;
+
+    newUser.firstName = paramsBody.firstName;
+    newUser.lastName = paramsBody.paramsBody;
+    newUser.email = paramsBody.email.toLowerCase();
+    newUser.role = "Rol-user";
+    newUser.image ="null";
+
+
+    if(paramsBody.password){
+        bcrypt.hash(paramsBody.password, null, null, function(err,hash){
+            newUser.password = hash;
+
+            if(newUser.firstName!=null && newUser.lastName !=null && newUser.email !=null){
+                if(User.findOne(newUser.email)){
+                    res.status(200).send({message:"El correo ya existe"});
+                    
+                }else{
+                    newUser.save((err,userCreate)=>{
+
+                        if(err){
+                            res.status(404).send({
+                                message:'Error al guardar usuario'
+                            
+                            });
+
+                        }else{
+                            if(!userCreate){
+                                res.status(400).send({message:"No se ha registrado el usuario"});
+                            }else{
+                                res.status(200).send({newUser:useCreate});
+                            }
+                        }
+                    });
+                }
+            }
+        
+
+        });
+
+
+
+    }else{
+        res.status(200).send({message:"Introduce la contraseña"})
+    }
+
+
+
+
 }
 
 module.exports= {
     pruebas,
+    create
     
 }
--- routers/user.js
+++ routers/user.js
@@ -3,7 +3,9 @@ const UserController = require('../controllers/user')
 
 const api = express.Router()
 
-api.get('/probando-controlador', UserController.pruebas)
 /* aqui se van a tener varias rutas */
+api.get('/probando-controlador', UserController.pruebas);
+api.post('/userRegister', UserController.create);
+
 
 module.exports = api;
